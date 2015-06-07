d8e553f2d0cccf0915c30cb145023d2b8b9f6a89
Richard Nguyen <therichardnguyen@gmail.com>
2015-06-06 22:43:53 -0700

    Finish the upload mechanics and tests

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,4 +4,32 @@ class Card < ActiveRecord::Base
   validates :name, presence: true
   validates :card_type, presence: true
   validates :version, presence: true
+  
+  def self.build_or_update_card_with_hash(hash, version="UNKNOWN")
+    attributes = {"version" => version}
+    hash.each do |key,value|
+      attributes[self.processed_key(key)] = value
+    end
+    begin
+      existing_card = Card.find(attributes["hearthstone_id"])
+      existing_card.assign_attributes(attributes)
+      return existing_card
+    rescue
+      begin
+        return Card.new(attributes)
+      rescue
+        return nil
+      end
+    end
+  end
+  
+  def self.processed_key(key)
+    processed_key = key.underscore
+    if (processed_key == "type")
+      processed_key = "card_type"
+    elsif (processed_key == "id")
+      processed_key = "hearthstone_id"
+    end
+    processed_key
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :cards, only: [:index]
+      post "upload", to: "upload#upload"
     end
   end
 end
